3d4caa9f99daf8dbffd09403b1f3c6b8ce0692e0
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-18 13:11:11 -0800

    Capturing progress on the queries

--- db/query.sql
+++ db/query.sql
@@ -6,3 +6,46 @@
 -- SELECT * FROM department;
 -- SELECT * FROM role;
 -- SELECT * FROM employee;
+
+
+-- View All Departments
+SELECT
+    department.id,
+    department.name AS department
+FROM
+    department
+ORDER BY
+    department.name ASC;
+
+
+-- View All Roles
+SELECT
+    role.id,
+    role.title,
+    department.name,
+    role.salary
+FROM
+    role
+JOIN 
+    department ON role.department_id = department.id
+ORDER BY
+    role.id ASC;
+
+
+-- View All Employees
+SELECT
+    employee.id,
+    employee.first_name,
+    employee.last_name,
+    role.title,
+    department.name AS department,
+    role.salary,
+    employee.manager_id
+FROM
+    employee
+JOIN 
+    role ON employee.role_id = role.id
+JOIN department
+    ON role.department_id = department.id
+ORDER BY
+    employee.id ASC;
