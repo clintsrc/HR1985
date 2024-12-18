e437792d00402a80b684d91e5835331f2c8aaa84
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-18 12:14:22 -0800

    SQL scripts for the fundamental schema and seeding

---
+++ db/query.sql
@@ -0,0 +1,8 @@
+-- ensure you're not connected to employees_db in caes it needs to be dropped
+\c employees_db
+-- helpful for troubleshooting your current connection
+\conninfo
+
+-- SELECT * FROM department;
+-- SELECT * FROM role;
+-- SELECT * FROM employee;
